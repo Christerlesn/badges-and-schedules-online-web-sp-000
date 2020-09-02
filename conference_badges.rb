84adf3cee892590583d7b4cdd4e059932ee05ead
Christerle Nounoute <christerlesn@gmail.com>
2020-09-02 19:39:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,3 @@
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(name)
-  name.each do |name|
-return "Hello, my name is #{name}"
-end
-end
-
-def batch_badge_creator
-end
-
-def assign_rooms
-end
-
-def printer
+def badge_maker(speaker)
+return "Hello, my name is #{speaker}."
 end
