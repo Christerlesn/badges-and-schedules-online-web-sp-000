4fd8304686a9592018d640a0bee3515be7e279d3
Christerle Nounoute <christerlesn@gmail.com>
2020-09-02 19:55:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,6 @@ def batch_badge_creator(speakers_lineup)
 end
 
 def assign_rooms(speakers_lineup)
-speakers_lineup.each_with_index {|speaker, room| speakers_lineup[room] = room}
+speakers_lineup.each_with_index {|speaker, room| speakers_lineup[speaker] = room}
 return "Hello, #{speaker}! You'll be assigned to room #{room}!"
 end
